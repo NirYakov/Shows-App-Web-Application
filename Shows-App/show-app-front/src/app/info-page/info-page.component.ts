c1b778b7b4d177265486119e31153435884626c8
Nir y <Niry27@gmail.com>
2023-06-27 05:20:55 +0300

    Same text added to explaine the app

--- Shows-App/show-app-front/src/app/info-page/info-page.component.ts
+++ Shows-App/show-app-front/src/app/info-page/info-page.component.ts
@@ -9,11 +9,39 @@ import { ActivatedRoute, Route, Router } from '@angular/router';
 })
 export class InfoPageComponent implements OnInit {
 
+  appOptionFeatures = [
+
+    {
+      title: "Save Your Shows",
+      content: "You can track the show you watched rate and review them."
+    },
+    {
+      title: "Add Friend",
+      content: "Search and add your friend"
+    },
+    {
+      title: "Me And Friend",
+      content: "You can see what your friend watched , find the same shows you watched and the difference shows"
+    },
+    {
+      title: "Edit your show (review, rating)",
+      content: "When you feel like you can edit your show (review, rating) to be most accrete"
+    },
+    {
+      title: "Play rating game (imdb)",
+      content: "Play shows game that you chose higher/lower rating of show vs other show"
+    },
+  ];
+
+
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
     private router: Router
   ) { }
+
+  appTitle = "Hey, series and movies lovers";
+  appSubTitle = "Website to tracking your Shows.";
 
   title = "Track Your Shows ( Movies & Tv's )"
   longText = "alot of text";
@@ -31,5 +59,56 @@ export class InfoPageComponent implements OnInit {
     }
 
   }
+
+  /*
+
+  title : "Save Your Shows",
+  content : "You can track the show you watched rate and review them."
+
+  title : "Add Friend",
+  content : "Search and add your friend"
+
+  title : "Me And Friend",
+  content : "You can see what your friend watched , find the same shows you watched and the difference shows"
+
+  title : "Play rating game (imdb)",
+  content : "Play shows game that you chose higher/lower rating of show vs other show"
+
+  title : "Edit your show (review,rating)",
+  content : "When you feel like you can edit your show (review,rating) to be most accrete"
+
+
+
+  */
+
+  /*
+
+
+  -----
+
+          Hello and welcome to Track Shows Web Application
+
+          -----
+
+
+
+Hi, series and movie lovers
+You are on a website tracking your plans.
+
+It is possible on the site to play a rating game according to IMDB.
+Adding a rating series/movie, and writing a comment.
+Adding friends to the app/site.
+Friends can see each other's series and also check the similarities and differences.
+
+Each user has a profile picture which is linked to a username, each name is unique and gives a unique image.
+
+If they have comments for me the key, I would love to hear improvement/preservation and new features and everything that comes up in the creative imagination.
+
+A pleasant stay on the site, Smile :)
+
+
+
+
+  */
 
 }
