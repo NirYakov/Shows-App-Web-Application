d54980fe1585266c11c8bc037aaab96925b6d655
Nir y <Niry27@gmail.com>
2023-06-29 10:13:26 +0300

    Added confirm password

--- Shows-App/show-app-front/src/app/auth/passwordValidator.ts
+++ Shows-App/show-app-front/src/app/auth/passwordValidator.ts
@@ -1,4 +1,5 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 export function createPasswordStrengthValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
@@ -27,4 +28,23 @@ export function createPasswordStrengthValidator(): ValidatorFn {
   }
 
 
+
+
+}
+
+
+
+export function ConfirmedValidator(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+    // if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+    //   return;
+    // }
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ confirmedValidator: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  }
 }
--- Shows-App/show-app-front/src/app/auth/signup/signup.component.ts
+++ Shows-App/show-app-front/src/app/auth/signup/signup.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, AbstractControlOptions, AbstractFormGroupDirective, FormBuilder, FormControl, FormGroup, UntypedFormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Subscription } from 'rxjs';
-import { createPasswordStrengthValidator } from '../passwordValidator';
+import { ConfirmedValidator, createPasswordStrengthValidator } from '../passwordValidator';
 
 @Component({
   selector: 'app-signup',
@@ -14,7 +14,7 @@ export class SignupComponent implements OnInit, OnDestroy {
   private authStatusSub: Subscription;
   form: FormGroup;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public fb: FormBuilder) { }
 
   ngOnDestroy(): void {
     this.authStatusSub.unsubscribe();
@@ -26,19 +26,33 @@ export class SignupComponent implements OnInit, OnDestroy {
         this.isLoading = false;
       });
 
-    this.form = new FormGroup({
+    // this.myForm = this.fb.group({
+    //   password: ['', [Validators.required]],
+    //   confirmPassword: ['']
+    // }, { validators: this.checkPasswords })
+
+    this.form = this.fb.group({
       // \w+(\w|\d)*@\w+(\w|\d)*\.\w+(\w|\d)
       // email: ['', Validators.required, Validators.pattern('\w+(\w|\d)*@\w+(\w|\d)*\.\w+(\w|\d)')],
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6), createPasswordStrengthValidator()]),
       username: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.pattern(`([A-Za-z0-9\-\_]+)`)]),
-    });
+      password: new FormControl(null, [Validators.required, Validators.minLength(6), createPasswordStrengthValidator()]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    }, { validator: ConfirmedValidator("password", "confirmPassword") });
+  }
+
+  checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    let pass = group.get('password').value;
+    let confirmPass = group.get('confirmPassword').value
+    return pass === confirmPass ? null : { notSame: true }
   }
 
   onSignup() {
     if (!this.form.valid) {
       return;
     }
+
+    console.log(this.form);
 
     this.isLoading = true;
     this.authService.createUser(this.form.value.email, this.form.value.username, this.form.value.password);
